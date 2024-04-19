06d839f2df88f695fa58b8344da3048333f20b8b
GHHBD Team <157905832+adnanfox123as@users.noreply.github.com>
2024-04-18 21:31:59 -0400

    Add files via upload

---
+++ 02.sh
@@ -0,0 +1,5 @@
+name="Adnan"
+bonso="Khan"
+
+echo "I know Your name is "$name 
+echo "And Your Nikename is "$bonso
---
+++ 03.sh
@@ -0,0 +1,17 @@
+clear
+# ai code ta 2 namber line a input nibe 
+echo "Enter your age :" 
+read age 
+echo $age
+
+#ai code ta same line a input nibe 
+read -p "Enter Your Name:" name
+echo "Your Name is: "$name
+
+
+#ai code gola Username,Password ar jonno
+
+read -p "Enter your Username: " usr
+read -p "Enter Your Password " psk
+
+echo $usr $psk
