363f076a69e368435102f505eb92f31fdaadaad7
GHHBD Team <157905832+adnanfox123as@users.noreply.github.com>
2024-04-25 01:07:58 -0400

    05.sh

---
+++ 05.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+read -p "Enter a Number: " nam1
+read -p "Enter another Number: " nam2
+echo ""
+
+echo "Your Plus Result is: $(( nam1 + nam2 ))"
+echo "Your Minus Result is: $(( nam1 - nam2 ))"
+echo "Your Gon Result is: $(( nam1 * nam2 ))"
+
+
